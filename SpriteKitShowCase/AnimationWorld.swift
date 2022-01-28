041f4a14eac728e41a4e52f79c8cd26ad268c3ca
张家豪 <zhangjiahao.me@bytedance.com>
2022-01-28 21:13:44 +0800

    Animation World step 1

--- SpriteKitShowCase/AnimationWorld.swift
+++ SpriteKitShowCase/AnimationWorld.swift
@@ -19,6 +19,11 @@ enum AnimationState: String {
     case inactive = "Inactive"
 }
 
+enum AnimationIdentifier: String {
+    case humanRun = "human_run"
+    case humanAttack = "human_attack"
+}
+
 /**
     Encapsulates all of the information needed to animate an entity and its shadow
     for a given animation state and facing direction.
@@ -28,33 +33,13 @@ struct Animation {
     // MARK: Properties
     
     /// The animation state represented in this animation.
-    let animationState: AnimationState
+    let identifier: AnimationIdentifier
     
     /// The direction the entity is facing during this animation.
-    let compassDirection: Direction
+    let direction: Direction
     
     /// One or more `SKTexture`s to animate as a cycle for this animation.
     let textures: [SKTexture]
-    
-    /**
-        The offset into the `textures` array to use as the first frame of the animation.
-        Defaults to zero, but will be updated if a copy of this animation decides to offset
-        the starting frame to continue smoothly from the end of a previous animation.
-    */
-    var frameOffset = 0
-    
-    /**
-        An array of textures that runs from the animation's `frameOffset` to its end,
-        followed by the textures from its start to just before the `frameOffset`.
-    */
-    var offsetTextures: [SKTexture] {
-        if frameOffset == 0 {
-            return textures
-        }
-        let offsetToEnd = Array(textures[frameOffset..<textures.count])
-        let startToBeforeOffset = textures[0..<frameOffset]
-        return offsetToEnd + startToBeforeOffset
-    }
 
     /// Whether this action's `textures` array should be repeated forever when animated.
     let repeatTexturesForever: Bool
@@ -64,15 +49,9 @@ struct Animation {
 
     /// The optional action for this entity's body, loaded from an action file.
     let bodyAction: SKAction?
-
-    /// The name of an optional action for this entity's shadow, loaded from an action file.
-    let shadowActionName: String?
-
-    /// The optional action for this entity's shadow, loaded from an action file.
-    let shadowAction: SKAction?
 }
 
-class AnimationWorld {
+class AnimationFactory {
     /// Returns the first texture in an atlas for a given `CompassDirection`.
     class func firstTexture(atlas atlas: SKTextureAtlas, imageIdentifier identifier: String) -> SKTexture {
         // Filter for this facing direction, and sort the resulting texture names alphabetically.
@@ -101,55 +80,39 @@ class AnimationWorld {
         }
     }
     
-    /// Creates an `Animation` from textures in an atlas and actions loaded from file.
-    class func animationsFromAtlas(atlas: SKTextureAtlas, withImageIdentifier identifier: String, forAnimationState animationState: AnimationState, bodyActionName: String? = nil, shadowActionName: String? = nil, repeatTexturesForever: Bool = true, playBackwards: Bool = false) -> [CompassDirection: Animation] {
-        // Load a body action from an actions file if requested.
+    class func animation(identifier identifier: AnimationIdentifier, bodyActionName: String? = nil, repeatTexturesForever: Bool = true) -> Animation {
+
         let bodyAction: SKAction?
         if let name = bodyActionName {
             bodyAction = SKAction(named: name)
-        }
-        else {
+        } else {
             bodyAction = nil
         }
-
-        // Load a shadow action from an actions file if requested.
-        let shadowAction: SKAction?
-        if let name = shadowActionName {
-            shadowAction = SKAction(named: name)
-        }
-        else {
-            shadowAction = nil
-        }
-        
-        /// A dictionary of animations with an entry for each compass direction.
-        var animations = [CompassDirection: Animation]()
-        
-        for compassDirection in CompassDirection.allDirections {
             
-            // Find all matching texture names, sorted alphabetically, and map them to an array of actual textures.
-            let textures = atlas.textureNames.filter {
-                $0.hasPrefix("\(identifier)_\(compassDirection.rawValue)_")
-            }.sorted {
-                playBackwards ? $0 > $1 : $0 < $1
-            }.map {
-                atlas.textureNamed($0)
-            }
-            
-            // Create a new `Animation` for these settings.
-            animations[compassDirection] = Animation(
-                animationState: animationState,
-                compassDirection: compassDirection,
-                textures: textures,
-                frameOffset: 0,
-                repeatTexturesForever: repeatTexturesForever,
-                bodyActionName: bodyActionName,
-                bodyAction: bodyAction,
-                shadowActionName: shadowActionName,
-                shadowAction: shadowAction
-            )
-            
+        // Find all matching texture names, sorted alphabetically, and map them to an array of actual textures.
+        let textures = atlas.textureNames.filter {
+            $0.hasPrefix("\(identifier)_\(compassDirection.rawValue)_")
+        }.sorted {
+            playBackwards ? $0 > $1 : $0 < $1
+        }.map {
+            atlas.textureNamed($0)
         }
         
-        return animations
+        // Create a new `Animation` for these settings.
+        let animation = Animation(
+            animationState: animationState,
+            direction: compassDirection,
+            textures: textures,
+            frameOffset: 0,
+            repeatTexturesForever: repeatTexturesForever,
+            bodyActionName: bodyActionName,
+            bodyAction: bodyAction
+        )
+    
+        return animation
     }
+}
+
+struct AnimationWorld {
+    
 }
